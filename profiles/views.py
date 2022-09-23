227040c2b08d7653ab4808cbca2f1da771be9d04
Mycrosys <Ryoga_Hibiki@gmx.de>
2022-09-23 12:59:08 +0000

    Feat: add following_count filter to profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from qa_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -10,9 +11,17 @@ class ProfileList(generics.ListAPIView):
     There is no need for a create view, because its handled by django signals
     """
 
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        following_count=Count('owner__follower', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
-
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'following_count',
+        'owner__follower__created_at',
+    ]   
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
@@ -22,5 +31,7 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
 
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        following_count=Count('owner__follower', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
